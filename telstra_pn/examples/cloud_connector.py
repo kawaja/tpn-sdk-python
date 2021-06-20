096c051f2d6a001bac79b138c28546379a79ccc9
Rik Harris <git@kawaja.net>
2021-06-20 19:08:05 +1000

    align syntax

--- telstra_pn/examples/cloud_connector.py
+++ telstra_pn/examples/cloud_connector.py
@@ -5,26 +5,26 @@ tpns = telstra_pn.Session(accountid='34324',
                           username='a@b.com',
                           password='154jk43l')
 
-alibaba = tpns.cloud.providers('Alibaba Cloud Express Connect')
+alibaba = tpns.clouds.providers['Alibaba Cloud Express Connect']
 
 e1 = tpns.clouds.create(
     cloud_provider=alibaba,
-    region=alibaba.regions('cn-hongkong'),
+    region=alibaba.regions['cn-hongkong'],
     bandwidth_mbps=50,
-    )
+)
 
 e2 = tpns.switchports.create(
     datacentre=tpns.datacentres['SYEQ'],
     interface_type='1000Base-TX'
-    )
+)
 
 flow = tpns.p2plinks.create(
     endpoints=[e1, e2],
     latency=latency.low,
-    durationHours=3600,
-    bandwidthMbps=1000,
-    renewalOption=renewal.auto_renew,
-    topology=tpns.topologies('testtopo')
+    duration_hours=3600,
+    bandwidth_mbps=1000,
+    renewal_option=renewal.auto_renew,
+    topology=tpns.topologies['testtopo']
 )
 
 print(f'number of contracts: {len(flow.active_contracts)}')
