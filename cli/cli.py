2f49503b2185b7e5facc125b7273bcbc84f38da8
Rik Harris <git@kawaja.net>
2021-09-25 23:03:39 +1000

    more robust with missing attributes

--- cli/cli.py
+++ cli/cli.py
@@ -32,12 +32,13 @@ class CLI:
         if names is None:
             names = [(x, x) for x in data.keys()]
         for (name, key) in names:
-            cprint(f'{name}: {data[key]}')
+            disp = data.get(key, '<unknown>')
+            cprint(f'{name}: {disp}')
 
     def output_single_json(self, data: dict) -> None:
         outitem = {}
         for (name, key) in self.names:
-            outitem[name] = data[key]
+            outitem[name] = data.get(key, '<unknown>')
 
         cprint(json.dumps(outitem))
 
@@ -52,11 +53,11 @@ class CLI:
 
     def output_list_text(self, data: dict) -> None:
         widths = {name[1]: len(name[0]) for name in self.names}
-        print(widths)
         for item in data:
             for (name, key) in self.names:
-                if widths[key] < len(str(item[key])):
-                    widths[key] = len(str(item[key]))
+                disp = str(item.get(key, '<unknown>'))
+                if widths[key] < len(disp):
+                    widths[key] = len(disp)
 
         if self.ctx.headers:
             output = ''
@@ -72,7 +73,8 @@ class CLI:
         for item in data:
             output = ''
             for (name, key) in self.names:
-                output += '{item:<{width}} '.format(item=str(item[key]),
+                disp = str(item.get(key, '<unknown>'))
+                output += '{item:<{width}} '.format(item=disp,
                                                     width=widths[key])
             cprint(output)
 
@@ -81,7 +83,7 @@ class CLI:
         for item in data:
             outitem = {}
             for (name, key) in self.names:
-                outitem[name] = str(item[key])
+                outitem[name] = str(item.get(key, '<unknown>'))
             outdata.append(outitem)
 
         cprint(json.dumps(outdata))
